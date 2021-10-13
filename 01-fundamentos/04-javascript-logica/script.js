f3afc0c09c5bc67102b7d21c589e10e6f245864f
Luis Monte <luismonte.l560@gmail.com>
2021-10-13 15:26:11 -0300

    adiciona arquivo JS, com resoluçao do primeiro exercicio

---
+++ 01-fundamentos/04-javascript-logica/script.js
@@ -0,0 +1,17 @@
+//exercicio 01
+const a = 2;
+const b = 4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+//exercicio 02
+if (a > b){
+    console.log(a);
+}
+else{
+    console.log(b);
+}
