a722449fb38607ed6bf13e241570b77b133fd911
Luís Monte <luismonte.l560@gmail.com>
2021-11-02 22:23:54 -0300

    exericios/6.1
    Adiciona todos os estados via JS.

---
+++ 01-fundamentos/06-forms-flexbox-responsivo/6.1/script.js
@@ -0,0 +1,8 @@
+const listaEstados = document.getElementById('estado');
+let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+
+for (let i = 0; i < estados.length; i += 1) {
+  const criaEstados = document.createElement('option');
+  listaEstados.appendChild(criaEstados);
+  criaEstados.innerText = estados[i];
+}
